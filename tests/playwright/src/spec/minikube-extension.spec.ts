47329b2caf186640814aa48e6a8d18ffdc20c702
Anton Misskii <amisskii@redhat.com>
2025-05-14 11:23:42 +0200

    fix: eof
    Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- tests/playwright/src/spec/minikube-extension.spec.ts
+++ tests/playwright/src/spec/minikube-extension.spec.ts
@@ -254,3 +254,4 @@ test.describe.serial('Podman Desktop Minikube Extension Tests', () => {
     await playExpect.poll(async () => await extensions.extensionIsInstalled(EXTENSION_LABEL), { timeout: 15000 }).toBeFalsy();
   });
 });
+ 
